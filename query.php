e8be5f21b989031ec7497c1371c03279bdf00860
silkslime <silkslime@yandex.ru>
2021-05-26 23:26:32 +0300

    d

--- query.php
+++ query.php
@@ -1,6 +1,6 @@
 <?php
-// $dbconn = pg_connect(getenv("DATABASE_URL"));
 $dbconn = pg_connect("host=ec2-54-78-36-245.eu-west-1.compute.amazonaws.com port=5432 dbname=damcbovdvkucji user=nwuwhicdfqfxys password=d297f268ecbb669c7af029b742c96d2421097ec0efdb871761d443b44dbae886");
+$dbconn = pg_connect(getenv("DATABASE_URL"));
 
 $id = $_GET["id"];
 $type = $_GET["type"];
